2f54a17d834c4fd50df6522f12645ca4cd9cd3b8
Dawid Lubowiecki <lubowiecki@gmail.com>
2022-03-28 15:45:27 +0200

    websocket

--- cypress/integration/frontend/pages/home.ts
+++ cypress/integration/frontend/pages/home.ts
@@ -1,6 +1,7 @@
-describe('Frontend', () => {
+describe('Frontend :: Home', () => {
 	it('Visits the initial project page', () => {
 		cy.visit('/');
+		cy.get('input[formControlName="firstname"]').should('have.value', 'firstname');
 		cy.checkScreenshot('home', 'pl');
 	});
 });
