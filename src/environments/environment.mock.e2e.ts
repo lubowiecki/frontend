ce3dcc72638d94705021154b84d610dce660ec44
Dawid Lubowiecki <lubowiecki@gmail.com>
2022-04-12 13:33:33 +0200

    config

--- src/environments/environment.mock.e2e.ts
+++ src/environments/environment.mock.e2e.ts
@@ -1,4 +1,5 @@
 export const environment = {
 	production: false,
 	restUri: 'http://host.docker.internal:4011',
+	webSocketUri: 'ws://localhost:8080/v1',
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
 	production: true,
 	restUri: 'http://localhost:4011',
+	webSocketUri: 'ws://localhost:8080/v1',
 };
