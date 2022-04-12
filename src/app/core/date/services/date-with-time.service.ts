4f4bac24ecb5ccacdda6d589261b249cc0176583
Dawid Lubowiecki <lubowiecki@gmail.com>
2022-04-12 09:30:09 +0200

    fix path

--- src/app/core/date/services/date-with-time.service.ts
+++ src/app/core/date/services/date-with-time.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { combineLatest, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { Translator } from 'src/app/core/translation/services/translator.service';
+
+import { Translator } from '@core/translation/services/translator.service';
 
 import { IsoDateWithTime } from '../value-objects/iso-date-with-time';
 
