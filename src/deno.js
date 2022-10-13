53f356f981d24e857d475018fb8e1c70628ffec1
Dawid Lubowiecki <lubowiecki@gmail.com>
2022-10-13 12:35:23 +0200

    github-actions-update

--- src/deno.js
+++ src/deno.js
@@ -1,18 +1,16 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
 async function handleRequest(request) {
-	const { pathname } = new URL(request.url);
-
 	// This is how the server works:
 	// 1. A request comes in for a specific asset.
 	// 2. We read the asset from the file system.
 	// 3. We send the asset back to the client.
 
-	// Check if the request is for index.html.
-	if (pathname.startsWith("/index.html")) {
-		// Read the index.html file from the file system.
-		const file = await Deno.readFile("./index.html");
-		// Respond to the request with the index.html file.
+	// Check if the request is for html.
+	if (request.url.match(/.html$/)) {
+		// Read the html file from the file system.
+		const file = await Deno.readFile(request.url);
+		// Respond to the request with the html file.
 		return new Response(file, {
 			headers: {
 				"content-type": "text/html",
@@ -20,9 +18,31 @@ async function handleRequest(request) {
 		});
 	}
 
+	if (request.url.match(/.css$/)) {
+		const file = await Deno.readFile(request.url);
+		return new Response(file, {
+			headers: {
+				"content-type": "text/css",
+			},
+		});
+	}
+
+	if (request.url.match(/.ico$/)) {
+		const file = await Deno.readFile(request.url);
+		return new Response(file, {
+			headers: {
+				"content-type": "image/x-icon",
+			},
+		});
+	}
+
 	const file = await Deno.readFile(request.url);
 
-	return new Response(file);
+	return new Response(file, {
+		headers: {
+			"content-type": "text/html; charset=utf-8",
+		},
+	});
 }
 
 serve(handleRequest);
