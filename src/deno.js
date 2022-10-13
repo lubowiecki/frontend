98d61b6380026decaec4bb14d8fe802de5bc74c2
Dawid Lubowiecki <lubowiecki@gmail.com>
2022-10-13 12:17:13 +0200

    github-actions

--- src/deno.js
+++ src/deno.js
@@ -1,5 +1,24 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
-const file = await Deno.readFile(".");
+async function handleRequest(request) {
+	const { pathname } = new URL(request.url);
 
-serve(async () => new Response(file));
+	// This is how the server works:
+	// 1. A request comes in for a specific asset.
+	// 2. We read the asset from the file system.
+	// 3. We send the asset back to the client.
+
+	// Check if the request is for index.html.
+	if (pathname.startsWith("/index.html")) {
+		// Read the index.html file from the file system.
+		const file = await Deno.readFile("./index.html");
+		// Respond to the request with the index.html file.
+		return new Response(file, {
+			headers: {
+				"content-type": "text/html",
+			},
+		});
+	}
+}
+
+serve(handleRequest);
