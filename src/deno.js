d6ffb56e19200233fc839a31d1cd9f62ff9b94a1
Dawid Lubowiecki <lubowiecki@gmail.com>
2022-10-13 11:55:30 +0200

    github-actions

---
+++ src/deno.js
@@ -0,0 +1,12 @@
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+
+const HTML = await Deno.readFile("./index.html");
+
+serve(
+	async () =>
+		new Response(HTML, {
+			headers: {
+				"content-type": "text/html",
+			},
+		})
+);
