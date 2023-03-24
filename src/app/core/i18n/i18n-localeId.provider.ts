2b700f333cd64bccd2c7a4767a360c3a4364afb1
Dawid Lubowiecki <dawid.lubowiecki@opi.org.pl>
2023-03-24 13:20:35 +0100

    translations

--- src/app/core/i18n/i18n-localeId.provider.ts
+++ src/app/core/i18n/i18n-localeId.provider.ts
@@ -3,10 +3,6 @@ import { LOCALE_ID, Provider } from '@angular/core';
 import { I18nLocaleId } from './i18n-localeId';
 import { I18nService } from './i18n.service';
 
-export function myStringFactory(service: I18nService) {
-	return service.getCurrentLanguage().toDto;
-}
-
 export const I18nLocaleProvider: Provider = {
 	provide: LOCALE_ID,
 	useClass: I18nLocaleId,
